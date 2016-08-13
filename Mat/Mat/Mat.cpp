ae3f8384d24c805187af61a8a8b38abf3dc1a76a
Sunasity <sunfangxuan@126.com>
2016-08-13 20:47:41 +0800

    Mat

---
+++ Mat/Mat/Mat.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+
+//template <typename Dtype>
+namespace Arsenal{
+
+class Mat{
+public: 
+	Mat(int row, int column){
+		_row = row;
+		_column = column;
+		int 
+	}
+private:
+	float *MAT_DATA;
+	int _row, _column;
+
+}
+
+
+}
