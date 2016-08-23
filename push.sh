34424d25fe6f1cfd9d18a059abe2eed43180469c
fxsun <sunfangxuan@126.com>
2016-08-23 22:28:52 +0800

    Enhance the performance for 25%

--- push.sh
+++ push.sh
@@ -1,3 +1,3 @@
 git add .
-git commit -m "error"
+git commit -m "Enhance the performance for 25%"
 git push origin master
--- src/Mat.cpp
+++ src/Mat.cpp
@@ -3,6 +3,15 @@
 #include "common.hpp"
 
 #define ROW_DOMIN
+#define FOLDING_SIZE 16
+#define PE C.MAT_DATA[C_idx] += tmp * B.MAT_DATA[B_idx]; \
+		   B_idx ++; \
+		   C_idx ++;
+
+#define PU for (int i_para = 0; i_para < n / FOLDING_SIZE; i_para ++){ \
+			   C.MAT_DATA[C_idx] += tmp * B.MAT_DATA[B_idx]; \
+			   B_idx ++; \
+			   C_idx ++;}																
 
 namespace Arsenal{
 template <typename Dtype>
@@ -33,10 +42,37 @@ void Mat<Dtype>::MM_multiply(const Mat &A, const Mat &B, Mat &C, const int m, co
 	for (int i_m = 0; i_m < m; i_m ++){   //from naive to row-domin
 		for (int i_k = 0; i_k < k; i_k ++){
 			int A_idx = i_m * k + i_k;
-			Dtype tmp = A.MAT_DATA[A_idx];			
-			for (int i_n = 0, int B_idx = i_k * n, int C_idx = i_m * n; i_n < n; i_n ++, B_idx ++, C_idx ++){				
-				C.MAT_DATA[C_idx] += tmp * B.MAT_DATA[B_idx];
+			Dtype tmp = A.MAT_DATA[A_idx];
+			int B_idx = i_k * n; int C_idx = i_m * n;
+#if 1			
+			for (int i_n = 0; i_n <= (n / FOLDING_SIZE); i_n ++){
+				if (i_n   < (n / FOLDING_SIZE)){						
+					PE; PE; PE; PE;
+					PE; PE; PE; PE;
+					PE; PE; PE; PE;
+					PE; PE; PE; PE;
+				}
+				else{
+					for (int i_para = 0; i_para < n % FOLDING_SIZE; i_para ++){
+						C.MAT_DATA[C_idx] += tmp * B.MAT_DATA[B_idx];						
+						B_idx ++;
+						C_idx ++;
+					}
+				}				
 			}
+#endif
+#if 0
+			PU; PU; PU; PU; 
+			PU; PU; PU; PU; 
+			PU; PU; PU; PU; 
+			PU; PU; PU; PU; 
+
+			for (int i_para = 0; i_para < n % FOLDING_SIZE; i_para ++){
+				C.MAT_DATA[C_idx] += tmp * B.MAT_DATA[B_idx];						
+				B_idx ++;
+				C_idx ++;
+			}
+#endif
 		}
 	}
 #endif
